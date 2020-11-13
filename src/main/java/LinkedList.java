9eb36d843775366a2274cfc1ddb7f24b84e10005
levy <897895407@qq.com>
2020-11-13 20:31:00 +0800

    feat: linkedList

---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,182 @@
+public class LinkedList<T> implements List<T>{
+  private Node head = new Node(null);
+
+  private class Node {
+    private T data;
+    private Node next = null;
+
+    Node(T data) {
+      Node.this.data = data;
+    }
+  }
+
+  @Override
+  public boolean add(T element) {
+    Node node = new Node(element);
+    if(head.next == null) head.next = node;
+    else {
+      Node p = head.next;
+      while(p.next != null) {
+        p = p.next;
+      }
+      p.next = node;
+    }
+
+    return true;
+  }
+
+  @Override
+  public boolean insert(int index, T element) {
+    Node e = new Node(element);
+
+    // 负数也行，默认是在头插入
+    if (index <= 0) {
+      Node q = head.next;
+      head.next = e;
+      e.next = q;
+      return true;
+    }
+
+    Node p = head;
+    int i = 0;
+
+    while (p.next != null) {
+      if (i == index) {
+        Node q = p.next;
+        p.next = e;
+        e.next = q;
+        break;
+      }
+      p = p.next;
+      i++;
+    }
+
+    // tail
+    if (index >= i) {
+      p.next = new Node(element);
+    }
+
+    // 链表没有容量限制，insert 肯定默认是成功的
+    return true;
+  }
+
+  @Override
+  public boolean remove(int index) {
+    if (index < 0) return false;
+    //  节省一次遍历
+//    if (index >= this.size()) return false;
+
+    boolean result = false;
+    Node p = head;
+    int i = 0;
+
+    while (p.next != null) {
+      // 如果入参 index > this.size()，则 i 永远不会等于 index
+      if (i == index) {
+        p.next = p.next.next;
+
+        result = true;
+        break;
+      }
+      p = p.next;
+      i++;
+    }
+
+    return result;
+  }
+
+  public boolean remove(T element) {
+    boolean result = false;
+    Node p = head;
+
+    while (p.next != null) {
+      if (p.next.data.equals(element)) {
+        p.next = p.next.next;
+
+        result = true;
+        break;
+      }
+      p = p.next;
+    }
+    return result;
+  }
+
+  @Override
+  public boolean set(int index, T element) {
+    if (index < 0) return false;
+    boolean result = false;
+    Node p = head;
+    int i = 0;
+
+    while (p.next != null) {
+      if (i == index) {
+        p.next.data = element;
+        result = true;
+        break;
+      }
+      p = p.next;
+    }
+
+
+    return result;
+  }
+
+  @Override
+  public T get(int index) {
+    if (index < 0) return null;
+
+    T result = null;
+    Node p = head;
+    int i = 0;
+
+    while (p.next != null) {
+      if (i == index) {
+        result = p.next.data;
+        break;
+      }
+      p = p.next;
+    }
+
+    return result;
+  }
+
+  @Override
+  public int size() {
+    int result = 0;
+    Node p = head;
+    while (p.next != null) {
+      p = p.next;
+      result++;
+    }
+    return result;
+  }
+
+  @Override
+  public boolean contains(T element) {
+    boolean result = false;
+
+    Node p = head;
+    while (p.next != null) {
+      if(p.next.data.equals(element)) {
+        result = true;
+        break;
+      }
+      p = p.next;
+    }
+    return result;
+  }
+
+  @Override
+  public T[] toArray() {
+    int size = this.size();
+    T[] result = (T[])new Object[size];
+
+    Node p = head;
+    for (int i = 0; i < size; i++) {
+      result[i] = p.next.data;
+      p = p.next;
+    }
+
+    return result;
+  }
+}
---
+++ src/test/java/LinkedListTest.java
@@ -0,0 +1,76 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+  private List<Integer> linkedList;
+
+  @BeforeEach
+  void init() {
+    linkedList = new LinkedList<>();
+  }
+
+  @Test
+  @DisplayName("add/size/toArray/contains")
+  void test() {
+    assertFalse(linkedList.contains(new Integer(1)));
+    assertNull(linkedList.get(0));
+    assertNull(linkedList.get(1));
+
+    // side effect
+    linkedList.add(new Integer(1));
+
+    assertTrue(linkedList.contains(new Integer(1)));
+    assertEquals("[1]", Arrays.toString(linkedList.toArray()));
+    assertEquals(1, linkedList.size());
+
+  }
+
+  @Test
+  @DisplayName("set/get")
+  void test2() {
+    linkedList.add(new Integer(1));
+
+    assertEquals(1, linkedList.get(0));
+
+    // side effect
+    assertFalse(linkedList.set(-1, new Integer(2)));
+    assertTrue(linkedList.set(0, new Integer(0)));
+    assertFalse(linkedList.set(1, new Integer(1)));
+
+    assertEquals(1, linkedList.size());
+    assertEquals(0, linkedList.get(0));
+  }
+
+  @Test
+  void remove() {
+    linkedList.add(new Integer(0));
+    linkedList.add(new Integer(1));
+
+    assertFalse(linkedList.remove(-1));
+    assertFalse(linkedList.remove(2));
+    assertTrue(linkedList.remove(1));
+    assertEquals(1, linkedList.size());
+    assertEquals(0,linkedList.get(0));
+
+    assertTrue(linkedList.remove(new Integer(0)));
+    assertEquals(0, linkedList.size());
+    assertNull(linkedList.get(0));
+    assertNull(linkedList.get(1));
+
+  }
+
+  @Test
+  void insert() {
+    linkedList.insert(0, new Integer(1));
+    linkedList.insert(0, new Integer(0));
+    linkedList.insert(3, new Integer(2));
+
+    assertEquals(3, linkedList.size());
+    assertEquals("[0, 1, 2]", Arrays.toString(linkedList.toArray()));
+  }
+}
