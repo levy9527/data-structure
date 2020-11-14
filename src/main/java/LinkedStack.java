95c1e38f6400488db823625e6c2cb535e4d10a9a
levy <897895407@qq.com>
2020-11-14 17:33:53 +0800

    feat: 链式表栈

---
+++ src/main/java/LinkedStack.java
@@ -0,0 +1,27 @@
+public class LinkedStack<T> implements Stack<T>{
+  private List<T> list;
+
+  LinkedStack() {
+    list = new LinkedList<>();
+  }
+
+  @Override
+  public boolean push(T element) {
+    return list.insert(0, element);
+  }
+
+  @Override
+  public T pop() {
+    return list.remove(0);
+  }
+
+  @Override
+  public int size() {
+    return list.size();
+  }
+
+  @Override
+  public T[] toArray() {
+    return list.toArray();
+  }
+}
---
+++ src/test/java/LinkedStackTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedStackTest {
+
+  @Test
+  @DisplayName("push/pop/size/toArray")
+  void push() {
+    Stack<Integer> stack = new LinkedStack<>();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    assertTrue(stack.push(4));
+    assertEquals(4, stack.size());
+    assertEquals("[4, 3, 2, 1]", Arrays.toString(stack.toArray()));
+
+    assertEquals(4, stack.pop());
+    assertEquals(3, stack.size());
+    assertEquals("[3, 2, 1]", Arrays.toString(stack.toArray()));
+  }
+
+}
