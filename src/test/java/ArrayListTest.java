19f529241d3c3c5f29d1bce6affc15da9a654fe2
levy <897895407@qq.com>
2020-11-14 16:55:49 +0800

    chore: remove all unnecessary boxing

--- src/test/java/ArrayListTest.java
+++ src/test/java/ArrayListTest.java
@@ -15,7 +15,7 @@ class ArrayListTest {
   @BeforeEach
   void init(){
     arrayList = new ArrayList<>(5);
-    arrayList.add(new Integer(1));
+    arrayList.add(1);
   }
 
   @Test
@@ -30,7 +30,7 @@ class ArrayListTest {
   @Test
   @Order(2)
   void contains() {
-    assertTrue(arrayList.contains(new Integer(1)));
+    assertTrue(arrayList.contains(1));
 
     Map map = new HashMap();
     map.put("key", "value");
@@ -52,12 +52,12 @@ class ArrayListTest {
     assertNull(arrayList.get(11));
     assertNull(arrayList.get(1));
 
-    arrayList.set(1, new Integer(2));
-    arrayList.set(0, new Integer(0));
-    arrayList.set(2, new Integer(3));
-    arrayList.set(3, new Integer(4));
-    arrayList.set(4, new Integer(5));
-    arrayList.set(4, new Integer(6));
+    arrayList.set(1, 2);
+    arrayList.set(0, 0);
+    arrayList.set(2, 3);
+    arrayList.set(3, 4);
+    arrayList.set(4, 5);
+    arrayList.set(4, 6);
 
     assertEquals(5, arrayList.size());
     assertEquals(new String("[0, 2, 3, 4, 6]"),Arrays.toString(arrayList.toArray()) );
@@ -77,9 +77,9 @@ class ArrayListTest {
     arrayList = new ArrayList<>(3);
     assertNull(arrayList.get(2));
 
-    arrayList.add(new Integer(1));
-    arrayList.add(new Integer(2));
-    arrayList.add(new Integer(3));
+    arrayList.add(1);
+    arrayList.add(2);
+    arrayList.add(3);
     arrayList.remove(1);
 
     assertEquals(3, arrayList.get(1));
@@ -90,15 +90,15 @@ class ArrayListTest {
   @Order(5)
   void insert() {
     // 1 -> 0,1,2,3,
-    arrayList.insert(0, new Integer(0));
-    arrayList.insert(2, new Integer(3));
-    arrayList.insert(2, new Integer(2));
+    arrayList.insert(0, 0);
+    arrayList.insert(2, 3);
+    arrayList.insert(2, 2);
 
     assertEquals(4, arrayList.size());
     assertEquals(new String("[0, 1, 2, 3]"),Arrays.toString(arrayList.toArray()) );
 
-    arrayList.insert(4, new Integer(4));
-    arrayList.insert(4, new Integer(5));
+    arrayList.insert(4, 4);
+    arrayList.insert(4, 5);
     assertEquals(5, arrayList.size());
     assertEquals(new String("[0, 1, 2, 3, 4]"),Arrays.toString(arrayList.toArray()) );
   }
--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -17,14 +17,14 @@ class LinkedListTest {
   @Test
   @DisplayName("add/size/toArray/contains")
   void test() {
-    assertFalse(linkedList.contains(new Integer(1)));
+    assertFalse(linkedList.contains(1));
     assertNull(linkedList.get(0));
     assertNull(linkedList.get(1));
 
     // side effect
-    linkedList.add(new Integer(1));
+    linkedList.add(1);
 
-    assertTrue(linkedList.contains(new Integer(1)));
+    assertTrue(linkedList.contains(1));
     assertEquals("[1]", Arrays.toString(linkedList.toArray()));
     assertEquals(1, linkedList.size());
 
@@ -33,14 +33,14 @@ class LinkedListTest {
   @Test
   @DisplayName("set/get")
   void test2() {
-    linkedList.add(new Integer(1));
+    linkedList.add(1);
 
     assertEquals(1, linkedList.get(0));
 
     // side effect
-    assertFalse(linkedList.set(-1, new Integer(2)));
-    assertTrue(linkedList.set(0, new Integer(0)));
-    assertFalse(linkedList.set(1, new Integer(1)));
+    assertFalse(linkedList.set(-1, 2));
+    assertTrue(linkedList.set(0, 0));
+    assertFalse(linkedList.set(1, 1));
 
     assertEquals(1, linkedList.size());
     assertEquals(0, linkedList.get(0));
@@ -48,8 +48,8 @@ class LinkedListTest {
 
   @Test
   void remove() {
-    linkedList.add(new Integer(0));
-    linkedList.add(new Integer(1));
+    linkedList.add(0);
+    linkedList.add(1);
 
     assertFalse(linkedList.remove(-1));
     assertFalse(linkedList.remove(2));
@@ -57,7 +57,7 @@ class LinkedListTest {
     assertEquals(1, linkedList.size());
     assertEquals(0,linkedList.get(0));
 
-    assertTrue(linkedList.remove(new Integer(0)));
+    assertTrue(linkedList.remove(0));
     assertEquals(0, linkedList.size());
     assertNull(linkedList.get(0));
     assertNull(linkedList.get(1));
@@ -66,9 +66,9 @@ class LinkedListTest {
 
   @Test
   void insert() {
-    linkedList.insert(0, new Integer(1));
-    linkedList.insert(-1, new Integer(0));
-    linkedList.insert(3, new Integer(2));
+    linkedList.insert(0, 1);
+    linkedList.insert(-1, 0);
+    linkedList.insert(3, 2);
 
     assertEquals(3, linkedList.size());
     assertEquals("[0, 1, 2]", Arrays.toString(linkedList.toArray()));
