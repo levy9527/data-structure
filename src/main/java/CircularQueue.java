21e8d9cedd3b11b227939933fffbe21d1111fc8c
levy <897895407@qq.com>
2020-11-14 22:41:47 +0800

    feat: 循环队列

---
+++ src/main/java/CircularQueue.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+public class CircularQueue<T> implements Queue<T>{
+  private T[] array;
+  private int capacity;
+  private int head = 0;
+  private int tail = 0;
+
+  CircularQueue(int capacity) {
+    if(capacity <=0) throw new RuntimeException("capacity must > 0");
+
+    array = (T[])new Object[capacity];
+    this.capacity = capacity;
+  }
+
+  @Override
+  public boolean enqueue(T element) {
+    if(head == tail) {
+      if(!Objects.isNull(array[head])) return false;
+    }
+
+    // 取余数
+    array[tail] = element;
+    tail = ++tail % capacity;
+
+    return true;
+  }
+
+  @Override
+  public T dequeue() {
+    if(head == tail && Objects.isNull(array[head])) return null;
+
+    T result = array[head];
+    array[head] = null;
+    head = ++head % capacity;
+
+    return result;
+  }
+
+  @Override
+  public T head() {
+    return array[head];
+  }
+
+  @Override
+  public int size() {
+    if(tail > head) return tail - head;
+    else if (tail < head) return tail + capacity - head;
+    else if (Objects.isNull(array[head])) return 0;
+    return capacity;
+  }
+
+  @Override
+  public String toString() {
+    int size = this.size();
+    T[] clone = (T[])new Object[size];
+
+    for (int i = 0; i < size; i++) {
+      clone[i] = array[(head + i) % capacity];
+    }
+
+    return Arrays.toString(clone);
+  }
+}
---
+++ src/test/java/CircularQueueTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircularQueueTest {
+
+  @Test
+  @DisplayName("enqueue/dequeue/head/size")
+  void test(){
+    Queue<Integer> queue = new CircularQueue<>(3);
+
+    queue.enqueue(1);
+    queue.enqueue(2);
+
+    assertTrue(queue.enqueue(3));
+    assertFalse(queue.enqueue(4));
+    assertEquals(3, queue.size());
+
+    assertEquals(1, queue.dequeue());
+    assertEquals(2, queue.head());
+    assertEquals(2, queue.size());
+
+    assertEquals("[2, 3]", queue.toString());
+    queue.enqueue(4);
+    assertEquals("[2, 3, 4]", queue.toString());
+  }
+}
