67a161eb892f15e373a83d7b834ce46f164088fe
levy <897895407@qq.com>
2020-11-15 11:20:17 +0800

    practice: 进制转换练习题

--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -73,4 +73,36 @@ class LinkedListTest {
     assertEquals(3, linkedList.size());
     assertEquals("[0, 1, 2]", Arrays.toString(linkedList.toArray()));
   }
+
+  /**
+   * convert decimal system to other numeral systems
+   */
+  static int baseConversion(int decimal, int base) {
+    if (decimal < 0) throw new RuntimeException("decimal must >= 0");
+    if (base < 2) throw new RuntimeException("base must >= 2");
+    if (decimal == 0) return 0;
+
+    Stack<Integer> stack = new LinkedStack<>();
+    int quotient = decimal;
+
+    while (quotient != 0) {
+      stack.push(quotient % base);
+      quotient = (int) Math.floor(quotient / base);
+    }
+
+    // TODO 这里不能使用 Integer ?
+    Object[] array = stack.toArray();
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < array.length; i++) {
+      builder.append(array[i]);
+    }
+
+    return Integer.parseInt(builder.toString());
+  }
+
+  @Test
+  void testBaseConversion() {
+    assertEquals(0, LinkedListTest.baseConversion(0, 16));
+    assertEquals(151520, LinkedListTest.baseConversion(65312, 16));
+  }
 }
