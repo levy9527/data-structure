8536e007abcfb6eaf878a1e78213df7fa6e31ac9
levy <897895407@qq.com>
2020-11-29 18:58:06 +0800

    feat: HuffmanTree init

---
+++ src/main/java/HuffmanTree.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+public class HuffmanTree {
+  private HuffmanTree left;
+  private HuffmanTree right;
+  private HuffmanTree parent;
+  private int weight = 0;
+  private int[] array;
+
+  private HuffmanTree() {
+  }
+
+  public HuffmanTree(int[] array) {
+    if (array.length < 3) throw new RuntimeException("to construct a Huffman Tree needs at least 3 elements");
+    // 先排一下序, 让数组由小到大排列
+    this.array = Sort.insertion(array, SORT_TYPE.ASC);
+  }
+
+  /**
+   * 构造的技巧是，默认自己是左(右）子节点，然后重复添加右(左）子节点及父节点
+   */
+  public void build() {
+    weight = array[0];
+
+    HuffmanTree left = this;
+    HuffmanTree right;
+    HuffmanTree parent;
+
+    for (int i = 1; i < array.length; i++) {
+      right = new HuffmanTree();
+      right.weight = array[i];
+
+      parent = new HuffmanTree();
+      parent.weight = left.weight + right.weight;
+      parent.left = left;
+      parent.right = right;
+
+      left.parent = parent;
+      right.parent = parent;
+
+      left = parent;
+    }
+  }
+
+  /**
+   * @return 权重值数组由大到小排序的字符串
+   */
+  @Override
+  public String toString() {
+    LinkedList<Integer> list = new LinkedList<>();
+    // 以左子节点为起始点
+    list.add(weight);
+
+    HuffmanTree node = this;
+
+    while (!Objects.isNull(node.parent)) {
+      list.insert(0, node.parent.right.weight);
+      node = node.parent;
+    }
+
+    return Arrays.toString(list.toArray());
+  }
+}
+
---
+++ src/test/java/HuffmanTreeTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HuffmanTreeTest {
+
+  @Test
+  void build() {
+    HuffmanTree tree = new HuffmanTree(new int[]{5,7,2,13});
+    tree.build();
+
+    assertEquals("[13, 7, 5, 2]", tree.toString());
+  }
+}
