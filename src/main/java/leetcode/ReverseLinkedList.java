62db9ad1d5a8fedbe726edd13b87648df8ac5ed2
levy <897895407@qq.com>
2021-01-14 14:27:34 +0800

    feat: reverse linked list

--- src/main/java/leetcode/ReverseLinkedList.java
+++ src/main/java/leetcode/ReverseLinkedList.java
@@ -11,8 +11,15 @@ class ListNode {
   ListNode(int x) { val = x; }
 }
 
+/**
+ * https://leetcode-cn.com/problems/reverse-linked-list/
+ */
 public class ReverseLinkedList {
-  public ListNode reverseList(ListNode head) {
+  /**
+   * 不修改原对象
+   * T: O(n), S: O(n)
+   */
+  public ListNode reverseNoSideEffect(ListNode head) {
     if (Objects.isNull(head)) return null;
 
     ListNode p = head;
@@ -26,5 +33,37 @@ public class ReverseLinkedList {
     }
 
     return result;
+  }
+
+  /**
+   * 原地反转，修改了原对象
+   * T: O(n), S: O(1)
+   */
+  public ListNode reverseWithSideEffect(ListNode head) {
+    ListNode result = null,
+      p = head;
+
+    while (!Objects.isNull(p)) {
+      ListNode pNext = p.next;
+      p.next = result;
+      result = p;
+      p = pNext;
+    }
+
+    return result;
+  }
+
+  /**
+   * 递归调用
+   * T: O(n), S: O(n)
+   */
+  public ListNode reverseRecursively(ListNode head) {
+    if (Objects.isNull(head) || Objects.isNull(head.next)) return head;
+
+    ListNode node = reverseRecursively(head.next);
+    // 关键在于，要指向下一个节点，前一个节点必须是能拿到的
+    head.next.next = head;
+    head.next = null;
+    return node;
   }
 }
