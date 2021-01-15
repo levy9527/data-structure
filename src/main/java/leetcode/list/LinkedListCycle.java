a379dfde07e0b1fc10cc8222f07c0509f44d7412
levy <897895407@qq.com>
2021-01-15 13:35:07 +0800

    docs: 补充注释

--- src/main/java/leetcode/list/LinkedListCycle.java
+++ src/main/java/leetcode/list/LinkedListCycle.java
@@ -8,7 +8,9 @@ import java.util.Objects;
 public class LinkedListCycle {
   /**
    * 快慢指针，如果有环，一定会相遇
-   * T: O(n) S: O(1)
+   * https://zh.wikipedia.org/wiki/Floyd%E5%88%A4%E5%9C%88%E7%AE%97%E6%B3%95
+   * 若设节点S到P距离为 m，环C的长度为 n，则时间复杂度为 O(m+n)}
+   * 空间复杂度为 O(1)
    */
   public boolean hasCycle(ListNode head) {
     boolean result = false;
