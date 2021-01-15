eebb6efd6ca8d72646e2701e29d3037784c02ecc
levy <897895407@qq.com>
2021-01-15 16:04:39 +0800

    feat: 补充台阶问题

--- src/main/java/problem/Step.java
+++ src/main/java/problem/Step.java
@@ -14,6 +14,18 @@ public class Step {
     return step;
   }
 
+  /**
+   * 假如这里有 n 个台阶，每次你可以跨 1 个台阶或者 2 个台阶，请问走这 n 个台阶有多少种走法？
+   * 解题关键：假设其他台阶已经走完了，只剩下1个或2个台阶。
+   */
+  int countMethods(int steps) {
+    if (steps <= 0) return 0;
+    if (steps == 1) return 1;
+    if (steps == 2) return 2;
+    return countMethods(steps - 1) + countMethods(steps - 2);
+  }
+
+
   public static void main(String[] args) {
     Step step = new Step();
     System.out.println(step.howMuch());
