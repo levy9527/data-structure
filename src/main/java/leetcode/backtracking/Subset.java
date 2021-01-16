eceb01ba51c9efa9d5a1367ec1564187ee429ac7
levy <897895407@qq.com>
2021-01-16 18:27:43 +0800

    feat: 子集问题2

--- src/main/java/leetcode/backtracking/Subset.java
+++ src/main/java/leetcode/backtracking/Subset.java
@@ -1,30 +1,38 @@
 package leetcode.backtracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * https://leetcode-cn.com/problems/subsets/
+ * https://leetcode-cn.com/problems/subsets-ii/
  */
 public class Subset {
   public List<List<Integer>> subsets(int[] nums) {
     List<List<Integer>> result = new ArrayList<>();
     List<Integer> track = new ArrayList<>();
+    boolean[] used = new boolean[nums.length];
 
-    backTrack(nums, 0, track, result);
+    Arrays.sort(nums);
+    backTrack(nums, 0, used, track, result);
     return result;
   }
 
-  private void backTrack(int[] nums, int begin,
+  private void backTrack(int[] nums, int begin, boolean[] used,
                          List<Integer> track, List<List<Integer>> result) {
 
     result.add(new ArrayList<>(track));
     if (begin == nums.length) return;
 
     for (int i = begin; i < nums.length; i++) {
+      if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+      used[i] = true;
       track.add(nums[i]);
-      backTrack(nums, i + 1, track, result);
+      backTrack(nums, i + 1, used, track, result);
       track.remove(track.size() - 1);
+      used[i] = false;
     }
   }
 
