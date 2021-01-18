5a16a66de1c8af0e20919b418a130938d8ef6d77
levy <897895407@qq.com>
2021-01-18 13:55:10 +0800

    feat: 复制带随机指针的链表

---
+++ src/main/java/leetcode/list/CopyListWithRandomPointer.java
@@ -0,0 +1,89 @@
+package leetcode.list;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode-cn.com/problems/copy-list-with-random-pointer/
+ */
+public class CopyListWithRandomPointer {
+
+  /**
+   * 空间换时间
+   * T: O(n) S: O(1)
+   */
+  public ListNode copyRandomList(ListNode head) {
+    if (head == null) return head;
+
+    ListNode p = head,
+      copy;
+
+    // copy value and weaves list
+    while (p != null) {
+      ListNode pNext = p.next;
+      p.next = new ListNode(p.val);
+      p = p.next;
+      p.next = pNext;
+      p = p.next;
+    }
+
+    // random pointer
+    p = head;
+    while (p != null) {
+      copy = p.next;
+      ListNode random = p.random;
+
+      if (random == null) copy.random = null;
+      else {
+        copy.random = random.next;
+      }
+
+      p = copy.next;
+    }
+
+    // next pointer/split list
+    p = head;
+    ListNode copyHead = p.next;
+    while (p != null) {
+      copy = p.next;
+      p.next = copy.next;
+
+      if (p.next != null)
+        copy.next = p.next.next;
+
+      p = p.next;
+    }
+    return copyHead;
+  }
+
+
+
+  /**
+   * 利用 hashMap 建立 origin 与 copy 一一对应的关系。
+   * 关键点是，origin 作为key，因为它是不会变的
+   * T: O(n), S: O(n)
+   */
+  public ListNode copyRandomListWithMap(ListNode head) {
+    if (head == null) return null;
+    Map<ListNode, ListNode> map = new HashMap<>();
+    ListNode p = head;
+
+    // 生成节点
+    while (p != null) {
+      ListNode node = new ListNode(p.val);
+      map.put(p, node);
+      p = p.next;
+    }
+
+    // 建立指针
+    p = head;
+    while (p != null) {
+      ListNode copy = map.get(p);
+      copy.next = map.get(p.next);
+      copy.random = map.get(p.random);
+      p = p.next;
+    }
+
+    return map.get(head);
+  }
+}
--- src/main/java/leetcode/list/ListNode.java
+++ src/main/java/leetcode/list/ListNode.java
@@ -3,6 +3,8 @@ package leetcode.list;
 class ListNode {
   int val;
   ListNode next;
+  ListNode random;
+
   ListNode() {}
   ListNode(int val) { this.val = val; }
   ListNode(int val, ListNode next) { this.val = val; this.next = next; }
