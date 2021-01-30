fa763b6b431237b161e60981b7d1e19560bf754c
levy <897895407@qq.com>
2021-01-30 13:31:44 +0800

    feat: 二叉树是否平衡

--- src/main/java/leetcode/tree/MaxDepth.java
+++ src/main/java/leetcode/tree/MaxDepth.java
@@ -1,10 +1,33 @@
 package leetcode.tree;
 
-/**
- * https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
- * T: O(n) S: O(n)
- */
 public class MaxDepth {
+
+  /**
+   *
+   * https://leetcode-cn.com/problems/ping-heng-er-cha-shu-lcof/
+   * 关键在于 遍历的同时，求 depth
+   */
+  public boolean isBalanced(TreeNode root) {
+    return traverse(root) != -1;
+  }
+
+  private int traverse(TreeNode root) {
+    if (root == null) return 0;
+
+    int left = traverse(root.left);
+    if (left == -1) return -1;
+
+    int right = traverse(root.right);
+    if (right == -1) return -1;
+
+    return Math.abs(left - right) > 1 ? -1 : 1 + Math.max(left, right);
+  }
+
+
+  /**
+   * https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+   * T: O(n) S: O(logn)
+   */
   public int maxDepth(TreeNode root) {
     if (root == null) return 0;
 
