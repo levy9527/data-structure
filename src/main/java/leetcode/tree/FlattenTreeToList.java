2c2bad3289b55aa0e7b2df934b8d9f87a3023583
levy <897895407@qq.com>
2021-02-08 21:44:50 +0800

    feat: 将二叉树展开为链表

--- src/main/java/leetcode/tree/FlattenTreeToList.java
+++ src/main/java/leetcode/tree/FlattenTreeToList.java
@@ -7,6 +7,7 @@ public class FlattenTreeToList {
   public void flatten(TreeNode root) {
     if (root == null) return;
 
+    // 前序遍历里，可以做很多事！不仅仅只是交换一下指针
     if (root.left != null) {
       TreeNode node = root.right;
       root.right = root.left;
