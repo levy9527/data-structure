e26f807a8f3713d8ffc1c01b0809526fde795e1d
levy <897895407@qq.com>
2022-01-02 20:37:55 +0800

    feat: 数据流中的中位数

---
+++ src/main/java/leetcode/heap/MedianFinder.java
@@ -0,0 +1,54 @@
+package leetcode.heap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * https://leetcode-cn.com/problems/shu-ju-liu-zhong-de-zhong-wei-shu-lcof/submissions/
+ */
+public class MedianFinder {
+  private PriorityQueue<Integer> max;
+  private PriorityQueue<Integer> min;
+
+  public MedianFinder() {
+    max = new PriorityQueue<>(Comparator.comparingInt(x -> -x));
+    min = new PriorityQueue<>(Comparator.comparingInt(x -> x));
+  }
+
+  public void addNum(int num) {
+    if (min.size() == max.size()) {
+      max.add(num);
+      min.add(max.poll());
+    }
+    else {
+      min.add(num);
+      max.add(min.poll());
+    }
+  }
+
+  public double findMedian() {
+    if (max.size() == min.size()) return (double) (max.peek() + min.peek()) / 2;
+    return max.size() > min.size() ? max.peek() : min.peek();
+  }
+
+  public static void main(String[] args) {
+    MedianFinder medianFinder = new MedianFinder();
+    medianFinder.addNum(-1);
+    medianFinder.addNum(-2);
+    System.out.println(medianFinder.findMedian());
+
+    medianFinder.addNum(-3);
+    System.out.println(medianFinder.findMedian());
+
+    medianFinder.addNum(-4);
+    System.out.println(medianFinder.findMedian());
+
+    medianFinder.addNum(-5);
+    System.out.println(medianFinder.findMedian());
+  }
+}
+
+// 1 -> max 1
+// 2 -> min 2
+// 3 -> min 2,3
+// 0 -> max 1,0
