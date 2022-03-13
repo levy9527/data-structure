736c90d60ae261cb092d072d4feb4ebf68be40e1
levy <897895407@qq.com>
2022-03-13 18:25:45 +0800

    test: init SymbolGraphTest

---
+++ src/test/java/algs4/graph/SymbolGraphTest.java
@@ -0,0 +1,45 @@
+package algs4.graph;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class SymbolGraphTest {
+  private SymbolGraph symbolGraph;
+
+  @BeforeAll
+  void init() {
+    symbolGraph = new SymbolGraph("src/test/java/algs4/graph/routes.txt", ",");
+  }
+
+  @Test
+  void testSymbolGraph() {
+    String den = "D EN";
+    String hou = "H_OU";
+    String jfk = "JFK";
+
+    assertTrue(symbolGraph.contains(den));
+    assertTrue(symbolGraph.contains(hou));
+    assertTrue(symbolGraph.contains(jfk));
+
+    assertEquals(den, symbolGraph.name(symbolGraph.index(den)));
+    assertEquals(hou, symbolGraph.name(symbolGraph.index(hou)));
+    assertEquals(jfk, symbolGraph.name(symbolGraph.index(jfk)));
+
+    String jfkAdjArrayStr = "[ORD, ATL, MCO]";
+    List<String> jfkAdjList = new ArrayList<>(3);
+    Graph G = symbolGraph.G();
+
+    for (int w : G.adjacency(symbolGraph.index(jfk))) {
+      jfkAdjList.add(symbolGraph.name(w));
+    }
+
+    assertEquals(jfkAdjArrayStr, jfkAdjList.toString());
+  }
+}
