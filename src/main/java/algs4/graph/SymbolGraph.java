24c4846a818ef0ba838e1c2525e223dc0b8cb9fb
levy <897895407@qq.com>
2022-03-13 17:57:27 +0800

    feat: init SymbolGraph API

---
+++ src/main/java/algs4/graph/SymbolGraph.java
@@ -0,0 +1,26 @@
+package algs4.graph;
+
+public class SymbolGraph {
+  private Graph G;
+
+  public SymbolGraph(String filename, String delimiter) {
+
+  }
+
+  public Graph G() {
+    return G;
+  }
+
+  public boolean contains(String symbol) {
+    return false;
+  }
+
+  public int index(String symbol) {
+    return 0;
+  }
+
+  public String name(int index) {
+    return "";
+  }
+
+}
