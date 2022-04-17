cfcd753bcc93113f553bcf30dd1c02e92e593b4e
levy <897895407@qq.com>
2022-04-17 18:09:05 +0800

    feat: KruskalMST primary code

--- src/main/java/algs4/graph/mst/EdgeWeightedGraph.java
+++ src/main/java/algs4/graph/mst/EdgeWeightedGraph.java
@@ -1,7 +1,93 @@
 package algs4.graph.mst;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 // similar to undirected graph
 public class EdgeWeightedGraph {
-  public EdgeWeightedGraph(String filename) {
+  private static final int DEFAULT_CAPACITY = 10;
+
+  private Set<Integer> V;
+  private int E;
+  private List<LinkedList<Integer>> adj;
+
+  public EdgeWeightedGraph(int numberOfVertices) {
+    initContainer(numberOfVertices);
   }
+
+  public EdgeWeightedGraph(String filename) {
+    try {
+      Scanner scanner = new Scanner(new File(filename));
+      List<String> lines = new ArrayList<>(DEFAULT_CAPACITY);
+      while (scanner.hasNextLine()) {
+        lines.add(scanner.nextLine());
+      }
+
+      initContainer(lines.size());
+
+      for (String line : lines) {
+        String[] vertices = line.split(" ");
+        addEdge(Integer.parseInt(vertices[0]), Integer.parseInt(vertices[1]));
+      }
+
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void initContainer(int numberOfVertices) {
+    V = new HashSet<>();
+    adj = new ArrayList<>(numberOfVertices);
+    for (int i = 0; i < numberOfVertices; i++) {
+      adj.add(new LinkedList<>());
+    }
+  }
+
+  /**
+   * add edge v-x to this graph
+   * to make this method public, need to deal with IndexOutOfBoundsException
+   */
+  public void addEdge(int v, int w) {
+    // disallow parallel or self-loop
+    if (hasEdge(v, w) || v == w) return;
+
+    LinkedList<Integer> vertexV = adj.get(v);
+    vertexV.addFirst(w);
+
+    LinkedList<Integer> vertexW = adj.get(w);
+    vertexW.addFirst(v);
+    E++;
+
+    V.add(v);
+    V.add(w);
+  }
+
+  public boolean hasEdge(int v, int w) {
+    if (Objects.isNull(adj.get(v))) return false;
+
+    for (int x : adj.get(v)) {
+      if (x == w) return true;
+    }
+
+    return false;
+  }
+
+  public int V() {
+    return V.size();
+  }
+
+  public int E() {
+    return E;
+  }
+
+  public Iterable<EdgeWeighted> edges() {
+    return Collections.emptyList();
+  }
+
+  public Iterable<Integer> adjacency(int v) {
+    if (v >= adj.size() || v < 0) return Collections.EMPTY_LIST;
+    return adj.get(v);
+  }
+
 }
--- src/main/java/algs4/graph/mst/KruskalMST.java
+++ src/main/java/algs4/graph/mst/KruskalMST.java
@@ -1,17 +1,41 @@
 package algs4.graph.mst;
 
-public class KruskalMST implements MST{
-  public KruskalMST(EdgeWeightedGraph G) {
+import algs4.graph.UF;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class KruskalMST implements MST{
+  private List<EdgeWeighted> mst;
+
+  public KruskalMST(EdgeWeightedGraph G) {
+    PriorityQueue<EdgeWeighted> queue = new PriorityQueue<>();
+    for (EdgeWeighted edge : G.edges()) queue.add(edge);
+
+    UF uf = new UF(G.V());
+    // the MST only needs V-1 edges
+    int maxEdge = G.V() - 1;
+    mst = new ArrayList<>(maxEdge);
+    while (!queue.isEmpty() && mst.size() < maxEdge) {
+      EdgeWeighted edgeWeighted = queue.poll();
+      int p = edgeWeighted.either();
+      int q = edgeWeighted.other(p);
+
+      if (uf.connected(p, q)) continue;
+
+      uf.union(p, q);
+      mst.add(new EdgeWeighted(p, q, edgeWeighted.weighted()));
+    }
   }
 
   @Override
   public Iterable<EdgeWeighted> edges() {
-    return null;
+    return mst;
   }
 
   @Override
   public double weight() {
-    return 0;
+    return 0D;
   }
 }
